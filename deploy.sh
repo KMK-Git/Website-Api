3c26225477520d5559361c5d30eb05c3cd3ce5e8
Kaustubh <kaustubhkhavnekar@gmail.com>
2019-09-18 00:18:26 +0530

    Changed approach for parameter overrides to attempt to fix build errors

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env bash
-params=$(jq -r '.[] | [.ParameterKey, .ParameterValue] | join("=") | gsub("[ ]";"\\ ")' params.json)
-echo $params
-escaped_params=$(printf "%q " "$params")
-echo $escaped_params
-sam deploy --parameter-overrides $escaped_params --template-file build/packaged.yaml --stack-name website-api
+sam deploy --parameter-overrides $(cat parameter_overrides) --template-file build/packaged.yaml --stack-name website-api
