0abc2afa800e4b8d62350c10aa0a3389fbc6faf8
Kaustubh <kaustubhkhavnekar@gmail.com>
2019-09-18 00:34:02 +0530

    Revert "Changed approach for parameter overrides to attempt to fix build errors"
    This reverts commit 3c26225477520d5559361c5d30eb05c3cd3ce5e8.

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-sam deploy --parameter-overrides $(cat parameter_overrides) --template-file build/packaged.yaml --stack-name website-api
+params=$(jq -r '.[] | [.ParameterKey, .ParameterValue] | join("=") | gsub("[ ]";"\\ ")' params.json)
+echo $params
+escaped_params=$(printf "%q " "$params")
+echo $escaped_params
+sam deploy --parameter-overrides $escaped_params --template-file build/packaged.yaml --stack-name website-api
