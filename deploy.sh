74ff22484b349151df7cdaad578361482d4a08e7
Kaustubh <kaustubhkhavnekar@gmail.com>
2019-09-17 22:33:17 +0530

    Added echo to debug

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 params=$(jq -r '.[] | [.ParameterKey, .ParameterValue] | join("=")' params.json)
+echo $params
 escaped_params=$(printf %q $params)
+echo $escaped_params
 sam deploy --parameter-overrides $escaped_params --template-file build/packaged.yaml --stack-name website-api
