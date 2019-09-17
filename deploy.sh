fc20d388a3c9d4df3d57d08024352ef3eccb32e2
Kaustubh <kaustubhkhavnekar@gmail.com>
2019-09-18 00:45:56 +0530

    Fixed template parameter

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 STACK_NAME="website-api"
+TEMPLATE="build/packaged.yaml"
 PARAMETERS_FILE="params.json"
 PARAMS=($(jq -r '.[] | [.ParameterKey, .ParameterValue] | "\(.[0])=\(.[1])"' ${PARAMETERS_FILE}))
 echo $PARAMS
 echo ${PARAMS[@]}
 sam deploy \
+  --template-file "${TEMPLATE}" \
   --stack-name "${STACK_NAME}" \
   --parameter-overrides ${PARAMS[@]}
