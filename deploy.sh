a9bdd19f2e10ab0d2741af3b813b2278bdf07177
Kaustubh <kaustubhkhavnekar@gmail.com>
2019-09-29 13:14:56 +0530

    Changed build process

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 aws s3 sync ./email_templates "s3://$EMAIL_TEMPLATE_BUCKET"
-aws s3 rm "s3://$BUILD_ARTIFACT_BUCKET" --recursive
+oldfiles=$(aws s3 ls s3://$BUILD_ARTIFACT_BUCKET --recursive | awk '{$1=$2=$3=""; printf "%s ", $0 }' | sed 's/[ \t]\+/ /g')
 sam package --template-file build/template.yaml --s3-bucket $BUILD_ARTIFACT_BUCKET --output-template-file build/packaged.yaml
 STACK_NAME="website-api"
 TEMPLATE="build/packaged.yaml"
@@ -11,3 +11,7 @@ sam deploy \
   --stack-name "${STACK_NAME}" \
   --parameter-overrides ${PARAMS[@]} \
   --no-fail-on-empty-changeset
+for file in ${oldfiles[*]}
+do
+	aws s3 rm "s3://$BUILD_ARTIFACT_BUCKET/$file" --dryrun
+done
