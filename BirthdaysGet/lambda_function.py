984e91810e73df447018d9385442d5c70dcf76bc
Kaustubh <kaustubhkhavnekar@gmail.com>
2019-10-28 18:56:46 +0530

    Added BirthdaysGet function

--- BirthdaysGet/lambda_function.py
+++ BirthdaysGet/lambda_function.py
@@ -1,57 +1,57 @@
 import logging
 import boto3
-import datetime
-import uuid
+import json
+import base64
 
 logger = logging.getLogger('logger')
 logger.setLevel(logging.DEBUG)
 
 
-def convert_event(event: dict) -> dict:
-    """
-    Converts event to dictionary of fields which will be saved to dynamodb.
-
-    :param event: Event which triggered Lambda. Contains form fields.
-    :return: Dictionary of fields.
-    """
-    # TODO: Check issues around leap year.
-    date_object = datetime.datetime(2020, event['month'], event['day'],
-                                    tzinfo=datetime.timezone(datetime.timedelta(hours=event['timezone'])))
-    utc_time_tuple = date_object.utctimetuple()
-    month, day = utc_time_tuple[1], utc_time_tuple[2]
-    hour, minute = utc_time_tuple[3], utc_time_tuple[4]
-    fields = {'FirstName': event['firstName'], 'LastName': event['lastName'], 'Email': event['email'],
-              'Subtext': event['subtext'], 'UnsubscribeKey': uuid.uuid4().hex,
-              'DateKey': '{:02d}-{:02d}'.format(month, day),
-              'TimeName': '{:02d}:{:02d}-{} {}'.format(hour, minute, event['firstName'], event['lastName'])}
-    return fields
+def convert_obj_to_b64_string(python_obj: object) -> str:
+    json_string = json.dumps(python_obj)  # Convert to JSON string.
+    json_bytes = bytes(json_string, 'utf-8')  # Convert string to bytes.
+    b64_bytes = base64.b64encode(json_bytes)  # Encode bytes to base64.
+    b64_string = b64_bytes.decode('utf-8')  # Convert bytes to string.
+    return b64_string
 
 
-def save_birthday_to_dynamodb(fields: dict) -> None:
-    """
-    Saves birthday to dynamodb, with proper time and an auto generated unsubscribe key.
+def convert_b64_string_to_obj(b64_string: str) -> object:
+    json_bytes = base64.b64decode(b64_string)  # Decode base64 string to bytes.
+    json_string = json_bytes.decode('utf-8')  # Convert bytes to string.
+    python_obj = json.loads(json_string)  # Convert json string to python object.
+    return python_obj
 
-    :param fields: Fields to be saved in dynamodb. This function assumes that all fields are strings.
-    :return: None.
+
+def get_birthdays(token: str = None) -> dict:
     """
-    item = {}
-    for key, value in fields.items():
-        item[key] = {'S': value}
-    dynamodb_client = boto3.client('dynamodb')
-    dynamodb_client.put_item(
-        TableName='BirthdaysTable',
-        Item=item
-    )
+    Gets list of all birthdays from DynamoDB. Uses pagination, results are limited to 20 per single call.
+
+    :param token: Token supplied for pagination. Use None if token not found.
+    :return: Dictionary result.
+             Key 'birthdays' is a list of birthdays.
+             Key 'token' indicates more birthdays are available and can be used for pagination.
+    """
+    result = {}
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('BirthdaysTable')
+    if token is not None:
+        decoded_token = convert_b64_string_to_obj(token)
+        scan_result = table.scan(Limit=20, ExclusiveStartKey=decoded_token)
+    else:
+        scan_result = table.scan(Limit=20)
+    result['birthdays'] = scan_result['Items']
+    if 'LastEvaluatedKey' in scan_result:
+        result['token'] = convert_obj_to_b64_string(scan_result['LastEvaluatedKey'])
+    return result
 
 
 def lambda_handler(event, context):
     logger.info(str(event).replace("'", '"'))
     try:
-        birthday_item = convert_event(event)
-        save_birthday_to_dynamodb(birthday_item)
+        token = None
+        if 'token' in event:
+            token = event['token']
+        return get_birthdays(token)
     except Exception:
         logger.exception("There was an exception")
         raise Exception("Malformed input")
-    return {
-        'message': 'Successful'
-    }
